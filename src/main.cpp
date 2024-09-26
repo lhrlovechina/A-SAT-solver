0f978ebb8ca0052577f069f8a993b70cecd62193
lhr <2200012733@stu.pku.edu.cn>
2024-09-26 16:48:01 +0800

    9.26_16:47

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include "ph.h"
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <string.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+
+    return 0;
+}
